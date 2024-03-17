09070073dc093678a811a377a77e321948461a0d
LucasDL11 <lucasdilandro@gmail.com>
2024-03-17 12:37:17 -0300

    Added dynamic routes, add product, edit product, delete product, same with cart

--- routes/shop.js
+++ routes/shop.js
@@ -9,10 +9,17 @@ const router = express.Router();
 router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
-router.get('/products/:productId', shopController.getProduct); //Dynamic segment, look for a value
+
+router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
+
+router.post('/cart', shopController.postCart);
+
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+
 router.get('/orders', shopController.getOrders);
+
 router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
