18633d0b0e4869e19b1c4cad21498fa3c47deaaa
LucasDL11 <lucasdilandro@gmail.com>
2024-03-17 17:45:33 -0300

    Full connection with SQL. Add edit and remove products, and for cart. Button order now added!

--- util/database.js
+++ util/database.js
@@ -1,2 +1,8 @@
-const mysql = require('mysql2')
+const Sequelize = require('sequelize');
 
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
+
+module.exports = sequelize;
